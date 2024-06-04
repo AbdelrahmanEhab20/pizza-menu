3f2f328f5d49e90e174e9477e87032b45e991953
AbdelrahmanEhab <abdoehab5008@gmail.com>
2024-06-04 21:33:36 +0300

    latest updates for splitting components + updates

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// function Header() {
+//   // const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+//   const style = {};
+// }
+function Header() {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+export default Header;
